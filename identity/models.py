120ba3fac3f72be75287eaa5ed1040ff91c7e51a
suhailshah115 <suhailzshah115@gmail.com>
2025-02-01 07:41:00 +0530

    Created contact model with email phone number primary key and created_at field

--- identity/models.py
+++ identity/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Contact(models.Model):
+	email = models.EmailField(null=True,blank=True)
+	phone = PhoneNumberField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	primary_contact = models.ForeignKey('self', null=True,
+		                                 blank=True, on_delete=models.SET_NULL,
+		                                 related_name= 'linked_contacts' )
+
+	def save(self,*args,**kwargs):
+		if not self.primary_contact:
+			self.primary_contact = self
+		super.save(*args,**kwargs)
